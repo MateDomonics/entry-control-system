687fbe2722279fcb6d72523d744b40c057d62e71
MateDomonics <domonicsmate@gmail.com>
2024-02-09 14:19:54 +0000

    Editing Website

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -5,17 +5,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Name: Máté Domonics
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Máté Domonics
+        </h1>
+        <h3>
+        BSc. (Hons) in Applied Computing (Cloud & Networks)
+        </h3>
+        <h1>KapU</h1>
+
+        
+        <h2>Project Poster:</h2>
+        <h3>T.B.D</h3>
+
+        <h2>Demo Video:</h2>
+        <h3>T.B.D</h3>
+        
+        <h2>Code Repository:</h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/MateDomonics/entry-control-system"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h3>GitHub Page</h3>
         </a>
       </header>
     </div>
