830a56d716939d83761de488d3f69db454d2299d
MateDomonics <domonicsmate@gmail.com>
2024-02-25 16:27:23 +0000

    Testing user creation on DynamoDB

--- python/main.py
+++ python/main.py
@@ -2,6 +2,8 @@ from scanner import Nfc
 import requests
 from hashlib import md5
 import atexit
+import user_creation
+from os import path
 
 #Use the md5 hash of a user's full name and email address.
 #This ensures a unique user ID.
@@ -15,19 +17,22 @@ api = "https://eooorfidlkf4wow.m.pipedream.net"
 database = {}
 users = {fake_user1: fake_user1_md5, fake_user2: fake_user2_md5}
 nfc_reader:Nfc = None
+user_manager = user_creation.User_manager.load_access(path.join(path.dirname(path.dirname(__file__)), "aws_access"))
 
 def callback(uuid: bytes) -> None:
     if uuid not in list(users.values()):
-        choice = input("Unknown card. Do you want to assign the card to a user? (y/n)")
+        choice = input("Unknown card. Do you want to create a new user? (y/n)")
         if choice.lower() not in ["y", "yes", "ye"]:
             return
         
-        print("Select a user")
-        for i, user in enumerate(list(users.keys())):
-            print(f"{i}: {user}")
-        index = int(input("Type in the number of the user you want to select."))
-        nfc_reader.write_data(list(users.values())[index])
-
+        new_user = user_creation.create_user()
+        #The UUID's hex value is converted into bytes, which is written onto the NFC tag.
+        nfc_reader.write_data(bytes.fromhex(new_user.uuid))
+        # print("Select a user")
+        # for i, user in enumerate(list(users.keys())):
+        #     print(f"{i}: {user}")
+        # index = int(input("Type in the number of the user you want to select."))
+        
         print("Finished.")
         return
     
---
+++ python/user_creation.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from uuid import uuid4
+import boto3
+from os import path
+
+@dataclass()
+class User():
+    uuid: str
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    active_subscription: bool
+    #https://stackoverflow.com/questions/68874635/why-is-my-python-dataclass-not-initialising-boolean-correctly
+    inside_facility: Optional[bool] = field(default=False)
+
+    
+class User_manager():
+    def __init__(self, aws_ak_id: str , aws_sak: str) -> None:
+        #https://stackoverflow.com/questions/48645867/how-to-establish-a-connection-to-dynamodb-using-python-using-boto3
+        self.client = boto3.client('dynamodb', aws_access_key_id=aws_ak_id, aws_secret_access_key=aws_sak, region_name= 'eu-west-1')
+        self.table_name = "KapU_DB"
+
+    @staticmethod
+    def load_access(file_path: str) -> "User_manager":
+        if not path.exists(file_path):
+            raise FileNotFoundError("Couldn't create User Manager. File not found.")
+        with open(file_path, "r") as fp:
+            data = fp.read(-1).split("\n")
+            if len(data) != 2:
+                raise Exception("Couldn't create User Manager. Wrong file format/content.")
+            return User_manager(aws_ak_id=data[0], aws_sak=data[1])
+
+    def create_user(self) -> User:
+        first_name = input("Please enter the customer's First Name: ")
+        last_name = input("Please enter the customer's Last Name: ")
+        email = input("Please enter the customer's email: ")
+        phone_number = input("Please enter the customer's Phone Number: ")
+        #UUID generates a UUID object, which is then converted into a hexadecimal number.
+        return User(uuid4().hex, first_name, last_name, email, phone_number, active_subscription=True)
+
+    #https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/dynamodb/client/put_item.html
+    def save_user(self, user: User) -> None:
+        self.client.put_item(
+            TableName = self.table_name,
+            Item = {
+                "uuid": {
+                    "S": user.uuid
+                },
+                "first_name": {
+                    "S": user.first_name
+                },
+                "last_name": {
+                    "S": user.last_name
+                },
+                "email": {
+                    "S": user.email
+                },
+                "phone_number": {
+                    "S": user.phone_number
+                },
+                "active_subscription": {
+                    "BOOL": user.active_subscription
+                },
+                "inside_facility": {
+                    "BOOL": user.inside_facility
+                }
+            }
+        )
