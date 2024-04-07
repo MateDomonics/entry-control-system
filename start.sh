9571d763a9526f74e313166374b438e0256eee8d
MateDomonics <domonicsmate@gmail.com>
2024-04-07 13:13:44 +0100

    Changed from bin/bash to bin/sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!bin/sh
 #https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR
